14d825d2d36a6d5ffc0e1b390549e98e00723533
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-07-18 13:03:43 +0530

    origin control resolutions

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
-	res.setHeader("Acces-Control-Allow-Orgin", req.headers.origin);
+	res.setHeader("Acces-Control-Allow-Orgin", "*");
 });
 
 app.get("/", (req, res) => {
